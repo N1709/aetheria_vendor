18297029586708dd1c89fc30e60a53c78b1383f7
kentaro aldanatien ramadhan <nkbob2707@gmail.com>
2026-07-17 13:38:50 +0700

    Update common_full_phone.mk

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -6,3 +6,4 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode?=true
 
 $(call inherit-product, vendor/lineage/config/telephony.mk)
+$(call inherit-product-if-exists, vendor/pixel/clocks/products/clocks.mk)
