c311d44c35d7e4c6de62c414c19bafad44616fd7
kentaro aldanatien ramadhan <nkbob2707@gmail.com>
2026-07-17 14:35:33 +0700

    Update common_full_phone.mk

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -5,6 +5,18 @@ $(call inherit-product, vendor/lineage/config/common_mobile_full.mk)
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode?=true
 
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.setupwizard.enterprise_mode=1 \
+    setupwizard.feature.baseline_setupwizard_enabled=true \
+    setupwizard.feature.day_night_mode_enabled=true \
+    setupwizard.feature.portal_notification=true \
+    setupwizard.feature.enable_quick_start_flow=true \
+    setupwizard.feature.lifecycle_refactoring=true \
+    setupwizard.feature.notification_refactoring=true \
+    setupwizard.feature.show_pixel_tos=true \
+    setupwizard.feature.joined_up_loading=true \
+    setupwizard.theme=glif_expressive
+
 $(call inherit-product, vendor/lineage/config/telephony.mk)
 $(call inherit-product-if-exists, vendor/pixel/clocks/products/clocks.mk)
 $(call inherit-product-if-exists, vendor/pixel/themepicker/products/themepicker.mk)
